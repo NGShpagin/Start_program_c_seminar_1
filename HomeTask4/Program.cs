5c57901ec8dee1a1e10f7c8308d45455d6a3cb73
NIKOLAY <shpagin9393@mail.ru>
2022-04-24 15:55:31 +0300

    add HomeTask4

---
+++ HomeTask4/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Print number: ");
+string number = Console.ReadLine();
+int num = int.Parse(number);
+
+int count = 1;
+
+while(count <= num)
+{
+    if(count % 2 == 0)
+    {
+        Console.Write(count);
+        Console.Write(", ");
+    }
+    count++;
+}
+Console.WriteLine();
+
+// Возможен другие варианты реализации решения данной задачи. например:
+
+//int count = 2;
+
+//while(count < num)
+//{
+//    Console.Write(count);
+//    Console.Write(", ");
+//    count = count + 2;
+//}
+//Console.WriteLine();
