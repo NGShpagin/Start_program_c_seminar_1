0a63d4849f2a91a87fa0e28eafe54eda5b473f3e
NIKOLAY <shpagin9393@mail.ru>
2022-04-24 15:39:35 +0300

    add HomeTask3

---
+++ HomeTask3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Print number: ");
+string number = Console.ReadLine();
+int num = int.Parse(number);
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("This number is integer");
+}
+
+else
+{
+    Console.WriteLine("This number is odd");
+}
